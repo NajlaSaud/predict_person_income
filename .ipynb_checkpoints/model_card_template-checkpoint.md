c2490314665c81b94f90fefb7878f2d3d8bffbd3
NajlaSaud <najlasaud1416@gmail.com>
2023-03-17 17:22:22 +0300

    model trainin files added

--- .ipynb_checkpoints/model_card_template-checkpoint.md
+++ .ipynb_checkpoints/model_card_template-checkpoint.md
@@ -1,18 +1,26 @@
 # Model Card
+Information about the machine learning model used in this project.
 
-For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
 
+This model is created by Najla Alsaedi. It is logistic regression using the default hyperparameters in scikit-learn 0.24.2.
+
 ## Intended Use
 
-## Training Data
+This model should be used to predict whether income exceeds $50K/yr based on census data.
 
-## Evaluation Data
+## Data
+The data was obtained from the UCI Machine Learning Repository (https://archive.ics.uci.edu/ml/datasets/Census+Income). 
+
+The original dataset has 32561 rows and 15 columns. After dropping the duplicate rows, it has 32537 rows. The data was breaked into train and test set with 80-20 split. 
+
+To use the data for training a One Hot Encoder was used on the features and a label binarizer was used on the labels.
 
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
 
-## Ethical Considerations
+The model was evaluated using:
+- Precision: The value is  0.7445
+- Recall: The recall is  0.2662
+- F beta score: The value is 0.3921
 
-## Caveats and Recommendations
--- starter/.ipynb_checkpoints/train_model-checkpoint.py
+++ starter/.ipynb_checkpoints/train_model-checkpoint.py
@@ -1,12 +1,17 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import pickle
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 
 # Add code to load in the data.
+data = pd.read_csv('./data/cleaned_data.csv')
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -19,10 +24,23 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print('precision %: ', precision)
+print('recall %: ', recall)
+print('fbeta %: ', fbeta)
+
+pickle.dump(model, open('./model/model.pkl','wb'))
+
